0608a8552323ad5f7c284add368213f727cb85a3
Silvonik <edwardmare101@gmail.com>
2026-07-31 22:39:21 +0300

    added about fixed for mobile hopefully and make random fact button

---
+++ script.js
@@ -0,0 +1,20 @@
+const randomFacts = [
+    "I live in Ireland",
+    "I play Basketball"
+];
+
+const fact = document.getElementById("fact");
+
+let lastIndex = -1;
+
+function randomFact() {
+
+    let randomIndex;
+
+    do {
+        randomIndex = Math.floor(Math.random() * randomFacts.length);
+    } while (randomIndex === lastIndex);
+
+    lastIndex = randomIndex;
+    fact.textContent = randomFacts[randomIndex];
+}
